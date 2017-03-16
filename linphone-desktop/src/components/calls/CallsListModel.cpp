9b13de1084bb3e8b3833b5c6eacd0239943957ac
Ronan Abhamon <ronan.abhamon@belledonne-communications.com>
2017-03-16 10:03:22 +0100

    feat(src/components/calls/CallsListModel): use a direction connection to deal with call states

--- linphone-desktop/src/components/calls/CallsListModel.cpp
+++ linphone-desktop/src/components/calls/CallsListModel.cpp
@@ -74,7 +74,9 @@ CallsListModel::CallsListModel (QObject *parent) : QAbstractListModel(parent) {
         default:
           break;
       }
-    }
+    },
+    // Necessary to avoid a segfault if a video stream exists.
+    Qt::DirectConnection
   );
 }
 
