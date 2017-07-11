b9f46d8d62b85c596ff4bd8add57cbe3a9e90a57
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2017-07-11 13:17:20 +0200

    Removed code to remove old oRTP submodules as it removes the right one on MacOSX because it's not case sensitive

--- prepare.py
+++ prepare.py
@@ -155,10 +155,6 @@ class DesktopPreparator(prepare.Preparator):
         if "python" in self.args.target or "python-raspberry" in self.args.target:
             ret |= not self.check_python_module_is_present('wheel')
 
-        if os.path.isdir('submodules/oRTP'):
-            import shutil
-            shutil.rmtree('submodules/oRTP')
-
         return ret
 
     def show_missing_dependencies(self):
