352e4985447f49b5eef7573d86377bbe3cf48537
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2018-03-16 17:26:18 +0100

    chore(build): add a rpm build support (in progress)

---
+++ build/rpm/linphone.spec.cmake
@@ -0,0 +1,70 @@
+# -*- rpm-spec -*-
+
+%define _prefix    @CMAKE_INSTALL_PREFIX@
+%define pkg_prefix @BC_PACKAGE_NAME_PREFIX@
+
+%define _datarootdir       %{_prefix}/share
+%define _datadir           %{_datarootdir}
+%define _docdir            %{_datadir}/doc
+
+%define build_number @PROJECT_VERSION_BUILD@
+%if %{build_number}
+%define build_number_ext -%{build_number}
+%endif
+
+Name:           @CPACK_PACKAGE_NAME@
+Version:        @PROJECT_VERSION@
+Release:        %{build_number}%{?dist}
+Summary:        A free (libre) SIP video-phone.
+
+Group:          Communications/Telephony
+License:        GPL
+URL:            https://www.linphone.org
+Source0:        %{name}-%{version}%{?build_number_ext}.tar.gz
+BuildRoot:      %{_tmppath}/%{name}-%{version}-%{release}-buildroot
+
+Requires:       %{pkg_prefix}bctoolbox
+Requires:       %{pkg_prefix}belcard
+Requires:       %{pkg_prefix}liblinphone
+Requires:       %{pkg_prefix}mediastreamer
+Requires:       %{pkg_prefix}minizip
+
+%description
+A free (libre) SIP video-phone.
+
+%if 0%{?rhel} && 0%{?rhel} <= 7
+%global cmake_name cmake3
+%define ctest_name ctest3
+%else
+%global cmake_name cmake
+%define ctest_name ctest
+%endif
+
+%define custom_debug_package %{!?_enable_debug_packages:%debug_package}%{?_enable_debug_package:%{nil}}
+%custom_debug_package
+
+%prep
+%setup -n %{name}-%{version}%{?build_number_ext}
+
+%build
+%{expand:%%%cmake_name} . -DCMAKE_BUILD_TYPE=@CMAKE_BUILD_TYPE@ -DCMAKE_INSTALL_LIBDIR:PATH=%{_libdir} -DCMAKE_PREFIX_PATH:PATH=%{_prefix} @RPM_ALL_CMAKE_OPTIONS@
+make %{?_smp_mflags}
+
+%install
+make install DESTDIR=%{buildroot}
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+%post -p /sbin/ldconfig
+%postun -p /sbin/ldconfig
+
+%files
+%defattr(-,root,root,-)
+%doc LICENSE CHANGELOG.md README.md
+%{_bindir}/
+%{_datadir}/
+
+%changelog
+* Thu Mar 15 2018 ronan.abhamon <ronan.abhamon@belledonne-communications.com>
+- Initial RPM release.
