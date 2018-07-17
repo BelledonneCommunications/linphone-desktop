9bcdc70cd816858622cc49d7fc9fb53412e40ee0
Ronan Abhamon <ronan.abhamon@belledonne-communications.com>
2018-07-17 12:18:08 +0200

    feat(CMakeLists.txt): do not deal with custom app_logo, replace it directly

--- assets/icons/genicons.sh
+++ assets/icons/genicons.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-if [ -f ../images/custom/app_logo.svg ] ; then
-  src=../images/custom/app_logo.svg
-else
-  src=../images/app_logo.svg
-fi
-
 for i in 16 22 24 32 64 128
 do
   mkdir -p hicolor/${i}x${i}/apps
-  inkscape -z -e hicolor/${i}x${i}/apps/icon.png -w $i -h $i $src
+  inkscape -z -e hicolor/${i}x${i}/apps/icon.png -w $i -h $i ../images/app_logo.svg
 done
