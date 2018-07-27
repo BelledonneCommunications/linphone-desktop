69e021395792fd06f1f73c06641d38caf595b3e3
Ronan Abhamon <ronan.abhamon@belledonne-communications.com>
2018-07-27 15:57:51 +0200

    feat(linphone_package): better code for deal with customizations (app name, url...) and packaging

--- application_info.cmake
+++ application_info.cmake
@@ -1,3 +1,6 @@
-set(EXECUTABLE_NAME linphone)
-set(APPLICATION_NAME Linphone)
 set(APPLICATION_DESCRIPTION "A libre SIP client")
+set(APPLICATION_NAME Linphone)
+set(APPLICATION_URL "https://www.linphone.org")
+set(APPLICATION_VENDOR "Belledonne Communications")
+
+set(EXECUTABLE_NAME linphone)
