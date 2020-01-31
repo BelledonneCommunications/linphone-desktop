447435fb746e19a7944345476b3fc72fcc801fd1
Julien Wadel <julien.wadel@belledonne-communications.com>
2020-01-31 16:30:18 +0100

    Update build_all_macos.sh [ci skip]

--- build_all_macos.sh
+++ build_all_macos.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
-
+################################################################################
+# CMakeLists.txt
+# Copyright (C) 2020  Belledonne Communications, Grenoble France
+#
+################################################################################
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+################################################################################
+#           MAC OSX First building script
+#-------------------------------------------------------------------------------
 #Stop at error
 set -e
 
@@ -15,10 +37,12 @@ mkdir -p submodules/externals/minizip/build-minizip
 
 #SDK building
 cd linphone-sdk/build-sdk
+#LINPHONESDK_DOXYGEN_PROGRAM is set just to be sure to get the version of the Application folder
 cmake .. -DLINPHONESDK_DOXYGEN_PROGRAM=/Applications/Doxygen.app/Contents/Resources/doxygen -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9
 cmake --build . --target all --parallel 5
 rsync -a linphone-sdk/desktop/ ../../build-desktop/OUTPUT/
 cd ../..
+
 #MiniZip Building
 cd submodules/externals/minizip/build-minizip
 cmake .. -DCMAKE_INSTALL_PREFIX=../../../../build-desktop/OUTPUT -DCMAKE_PREFIX_PATH=../../../build-desktop/OUTPUT
