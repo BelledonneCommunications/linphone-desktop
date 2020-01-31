7538f75e9f523cd07e6a70b8efe98e25e6fc1edd
Wadel Julien <julien.wadel@belledonne-communications.com>
2020-01-31 17:36:00 +0100

    Update icons 2/2

--- assets/icons/genicons.sh
+++ assets/icons/genicons.sh
@@ -3,5 +3,6 @@
 for i in 16 22 24 32 64 128
 do
   mkdir -p hicolor/${i}x${i}/apps
-  inkscape -z -e hicolor/${i}x${i}/apps/icon.png -w $i -h $i ../images/app_logo.svg
+  inkscape -z -e hicolor/${i}x${i}/apps/icon.png -w $i -h $i ../images/linphone_logo.svg
 done
+inkscape -z -e ../icon.ico -w 255 -h 255 ../images/linphone_logo.svg
