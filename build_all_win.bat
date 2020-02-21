6c109a2854924457ae544ba44691a38a2a877ce9
Julien Wadel <julien.wadel@belledonne-communications.com>
2020-02-21 23:37:59 +0100

    Change windows build

--- build_all_win.bat
+++ build_all_win.bat
@@ -9,14 +9,14 @@ cmake .. -DLINPHONESDK_PLATFORM=Desktop -DCMAKE_BUILD_TYPE=RelWithDebInfo -A Win
 :: Mini config
 ::cmake .. -DLINPHONESDK_PLATFORM=Desktop -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_VPX=NO -DENABLE_OPUS=NO -A Win32 -DENABLE_VIDEO=YES -DENABLE_GL=YES
 if %errorlevel% neq 0 exit /b %errorlevel%
-cmake --build . --target sdk --config RelWithDebInfo --parallel 10 -- /maxcpucount /nodeReuse:true /p:TrackFileAccess=false;DoNotCopyLocalIfInGac=true;CL_MPCount=16
+cmake --build . --target sdk --config RelWithDebInfo --parallel 10 -- /maxcpucount /nodeReuse:true /p:TrackFileAccess=false
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: Minizip Submodule Building
 cmake ..  -DCMAKE_BUILD_TYPE=RelWithDebInfo -A Win32
-cmake --build . --target install --config RelWithDebInfo --parallel 10 -- /maxcpucount /nodeReuse:true /p:TrackFileAccess=false;DoNotCopyLocalIfInGac=true;CL_MPCount=16
+cmake --build . --target install --config RelWithDebInfo --parallel 10 -- /maxcpucount /nodeReuse:true /p:TrackFileAccess=false
 
 :: Desktop Building
 cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -A Win32
 if %errorlevel% neq 0 exit /b %errorlevel%
-cmake --build . --target install --config RelWithDebInfo --parallel 10 -- /maxcpucount /nodeReuse:true /p:TrackFileAccess=false;DoNotCopyLocalIfInGac=true;CL_MPCount=16
+cmake --build . --target install --config RelWithDebInfo --parallel 10 -- /maxcpucount /nodeReuse:true /p:TrackFileAccess=false
