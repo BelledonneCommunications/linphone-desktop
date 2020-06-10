c2c60f5f1b3f9ffa82b6398c4e62be3558ea320a
Julien Wadel <julien.wadel@belledonne-communications.com>
2020-06-10 12:39:25 +0200

    - Do not do pause when removing a participant from the conference and let the SDK doing it.

--- linphone-app/src/components/conference/ConferenceAddModel.cpp
+++ linphone-app/src/components/conference/ConferenceAddModel.cpp
@@ -160,10 +160,10 @@ void ConferenceHelperModel::ConferenceAddModel::update () {
       while(address != allLinphoneAddresses.end() && (*address)->asStringUriOnly() != callAddress)
         ++address;
       if(address == allLinphoneAddresses.end()){// Not in conference list :  put in pause and remove it from conference if it's the case
-        call->pause();
         if( call->getParams()->getLocalConferenceMode() ){// Remove conference if it is not yet requested
           CoreManager::getInstance()->getCore()->removeFromConference(call);
-        }
+        }else
+          call->pause();
       }
     }
 }
