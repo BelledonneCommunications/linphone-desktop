0940fbb84da1c6a6ee9689bb8901ac284720aab8
Aude Marty <aude.marty@belledonne-communications.com>
2021-09-15 08:42:41 +0000

    Translated using Weblate
    Currently translated at 99.4% (780 of 784 strings)
    (French)
    
    Translation: Linphone Desktop/4.3
    Translate-URL: https://weblate.linphone.org/projects/linphone-desktop/4-3/fr/

--- linphone-app/assets/languages/fr_FR.ts
+++ linphone-app/assets/languages/fr_FR.ts
@@ -431,7 +431,7 @@
     <message>
         <source>attendedTransferCall</source>
         <extracomment>&apos;ATTENDED TRANSFER CALL&apos; : Title button, design is in uppercase.</extracomment>
-        <translation>TRANSFERER AVEC CONSULTATION</translation>
+        <translation>CONSULTATION AVANT TRANSFERT</translation>
     </message>
 </context>
 <context>
