a124d581e03342902384f6a7c3c597cb30556939
Gaelle Braud <gaelle.braud@belledonne-communications.com>
2026-02-24 15:02:02 +0100

    fix crash on restart due to null main window

--- Linphone/core/App.cpp
+++ Linphone/core/App.cpp
@@ -1322,10 +1322,9 @@ void App::setMainWindow(QQuickWindow *data) {
 		connect(mMainWindow, &QQuickWindow::activeChanged, this, &App::handleAppActivity);
 		handleAppActivity();
 
-		mMainWindow->winId();
-
 #ifdef Q_OS_WIN
-		WTSRegisterSessionNotification(reinterpret_cast<HWND>(mMainWindow->winId()), NOTIFY_FOR_THIS_SESSION);
+		if (mMainWindow)
+			WTSRegisterSessionNotification(reinterpret_cast<HWND>(mMainWindow->winId()), NOTIFY_FOR_THIS_SESSION);
 #endif
 
 		emit mainWindowChanged();
