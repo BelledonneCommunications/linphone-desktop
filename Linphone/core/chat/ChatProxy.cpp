e0044f71b318adc39c3eb993b2a55448460cb15f
Gaelle Braud <gaelle.braud@belledonne-communications.com>
2026-05-04 12:12:33 +0200

    do not sort chat list if searching with filter #LINQT-2511

--- Linphone/core/chat/ChatProxy.cpp
+++ Linphone/core/chat/ChatProxy.cpp
@@ -82,6 +82,7 @@ bool ChatProxy::addChatInList(ChatGui *chatGui) {
 }
 
 bool ChatProxy::lessThan(const QModelIndex &sourceLeft, const QModelIndex &sourceRight) const {
+	if (!mFilterText.isEmpty()) return false;
 	auto l = getItemAtSource<ChatList, ChatCore>(sourceLeft.row());
 	auto r = getItemAtSource<ChatList, ChatCore>(sourceRight.row());
 	if (l && r) return l->getLastUpdatedTime() > r->getLastUpdatedTime();
